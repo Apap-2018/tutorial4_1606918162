85bc43c8183cf47a9ad37b5386e319eb7b4c545c
Arief Fadli Wicaksono <arieffadliwicaksono@gmail.com>
2018-10-05 22:19:16 +0700

    first commit

---
+++ src/main/java/com/apap/tutorial4/controller/PilotController.java
@@ -0,0 +1,47 @@
+package com.apap.tutorial4.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apap.tutorial4.model.FlightModel;
+import com.apap.tutorial4.model.PilotModel;
+import com.apap.tutorial4.service.PilotService;
+
+@Controller
+public class PilotController {
+	@Autowired
+	private PilotService pilotService;
+	
+	@RequestMapping("/")
+	private String home() {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/pilot/add", method = RequestMethod.GET)
+	private String add(Model model) {
+		model.addAttribute("pilot", new PilotModel());
+		return "addPilot";
+	}
+	
+	@RequestMapping(value = "/pilot/add", method = RequestMethod.POST)
+	private String addPilotSubmit(@ModelAttribute PilotModel pilot) {
+		pilotService.addPilot(pilot);
+		return "add";
+	}
+	
+	@RequestMapping(value = "/pilot/view", method = RequestMethod.GET)
+	private String view (@RequestParam("licenseNumber") String licenseNumber, Model model) {
+		PilotModel pilot = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+		List<FlightModel> archive = pilot.getPilotFlight();
+		model.addAttribute("pilot", pilot);
+		return "view-pilot";
+	}
+
+}
---
+++ src/main/java/com/apap/tutorial4/service/FlightService.java
@@ -0,0 +1,9 @@
+package com.apap.tutorial4.service;
+
+import com.apap.tutorial4.model.FlightModel;
+
+public interface FlightService {
+	
+	void addFlight(FlightModel flight);
+
+}
